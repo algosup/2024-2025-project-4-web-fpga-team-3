7dbdb41fe7170a3a56051993ea02ff356fb8fa03
CamilleGayat <camille.gayat@algosup.com>
2025-03-26 14:48:30 +0100

    Merging TestingForParser & Parser
    > Merging TestingForParser & Parser
    
    > Modifying colors of simulation cubes

--- Web-FPGA/src/App.jsx
+++ Web-FPGA/src/App.jsx
@@ -12,53 +12,53 @@ function App() {
   const [cubeColors, setCubeColors] = useState({});
   const simulationCubeRef = useRef(null);
 
-	const handleFileChange = (event) => {
-		const fileInput = event.target.files[0];
-		setFile(fileInput);
-		setFileUploaded(false);
-		setError("");
-	};
+  const handleFileChange = (event) => {
+    const fileInput = event.target.files[0];
+    setFile(fileInput);
+    setFileUploaded(false);
+    setError("");
+  };
 
-	const handleUpload = async () => {
-		if (!file) {
-			setError("Please select an SDF file.");
-			return;
-		}
+  const handleUpload = async () => {
+    if (!file) {
+      setError("Please select an SDF file.");
+      return;
+    }
 
-		const reader = new FileReader();
-		reader.onload = async (event) => {
-			const sdfContent = event.target.result;
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const sdfContent = event.target.result;
 
-			try {
-				const parser = new SDFParser();
-				const parsedJson = parser.parseSDF(sdfContent);
-				const transformedJson = transformData(parsedJson);
+      try {
+        const parser = new SDFParser();
+        const parsedJson = parser.parseSDF(sdfContent);
+        const transformedJson = transformData(parsedJson);
 
-				localStorage.setItem(
-					"transformedJson",
-					JSON.stringify(transformedJson)
-				);
+        localStorage.setItem(
+          "transformedJson",
+          JSON.stringify(transformedJson)
+        );
 
-				setResult(transformedJson.nodes);
-				setFileUploaded(true);
-			} catch (error) {
-				setError("Error parsing or transforming the file.");
-			}
-		};
+        setResult(transformedJson.nodes);
+        setFileUploaded(true);
+      } catch (error) {
+        setError("Error parsing or transforming the file.");
+      }
+    };
 
-		reader.readAsText(file);
-	};
+    reader.readAsText(file);
+  };
 
   useEffect(() => {
     const colors = {};
     result.forEach(node => {
       if (node.id.toLowerCase().includes("lut")) { // Check for "lut" in cell instance
-        colors[`${node.type}-${node.id}`] = "#FF0000"; // Red color for "lut"
+        colors[`${node.type}-${node.id}`] = "#d3d3d3"; // Red color for "lut"
       }
       if (node.id.toLowerCase().includes("dff")) { // Check for "dff" in cell instance
-        colors[`${node.type}-${node.id}`] = "#008000"; // Green color for "dff"
+        colors[`${node.type}-${node.id}`] = "#47697a"; // Green color for "dff"
       } else if (node.id.toLowerCase().includes("latch")) { // If dff is not present, then check for "latch"
-        colors[`${node.type}-${node.id}`] = "#008000"; // Green color for "latch" (dff)
+        colors[`${node.type}-${node.id}`] = "#47697a"; // Green color for "latch" (dff)
       }
     });
     setCubeColors(colors);
@@ -75,43 +75,43 @@ function App() {
     return acc;
   }, {});
 
-	return (
-		<div className="app">
-			<header className={`top-bar ${fileUploaded ? "shifted" : ""}`}>
-				<div className="logo">FPGA Simulator</div>
-				<div className="upload-section">
-					<input
-						className="uploadButton"
-						type="file"
-						accept=".sdf"
-						onChange={handleFileChange}
-					/>
-					<button className="uploadButton" onClick={handleUpload}>Upload</button>
-				</div>
-			</header>
+  return (
+    <div className="app">
+      <header className={`top-bar ${fileUploaded ? "shifted" : ""}`}>
+        <div className="logo">FPGA Simulator</div>
+        <div className="upload-section">
+          <input
+            className="uploadButton"
+            type="file"
+            accept=".sdf"
+            onChange={handleFileChange}
+          />
+          <button className="uploadButton" onClick={handleUpload}>Upload</button>
+        </div>
+      </header>
 
-			<section className="main-content">
-				<div className="simulation-cube">
-					<h2>Simulation</h2>
-					<div className="columns-container">
-						{Object.keys(groupedNodes).map((type, idx) => (
-							<div key={idx} className="column">
-								<h3>{type}</h3>
-								<div className={`id-cubes-container -${idx + 1}`}>
-									{groupedNodes[type].map((node, nodeIdx) => (
-										<div
-											key={nodeIdx}
-											className={`id-cube -${nodeIdx + 1}`}
-											style={{ backgroundColor: cubeColors[`${type}-${node.id}`] || 'white' }}
-										>
-											{node.id}
-										</div>
-									))}
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
+      <section className="main-content">
+        <div className="simulation-cube" ref={simulationCubeRef}>
+          <h2>Simulation</h2>
+          <div className="columns-container">
+            {Object.keys(groupedNodes).map((type, idx) => (
+              <div key={idx} className="column">
+                <h3>{type}</h3>
+                <div className={`id-cubes-container -${idx + 1}`}>
+                  {groupedNodes[type].map((node, nodeIdx) => (
+                    <div
+                      key={nodeIdx}
+                      className={`id-cube -${nodeIdx + 1}`}
+                      style={{ backgroundColor: cubeColors[`${type}-${node.id}`] || 'white' }}
+                    >
+                      {node.id}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
         <div className="logs-cube">
           <h2>Logs</h2>
           <table className="logs-table">
